0e0d312479236edc019caa18ea7534fc6c2f9881
sohei-fujita <nnn.sohei.fujita@gmail.com>
2025-05-09 08:40:25 +0900

    Initial commit

---
+++ backend/blog/views.py
@@ -0,0 +1,10 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+
+# backend/blog/views.py
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello from blog app")
